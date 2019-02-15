d1ffe1f8bf27e9e9c30e3b1de2a48517ed0ff4c9
jsakowski <jsakowski@it.ucla.edu>
2019-02-15 12:12:09 -0800

    refactor: Leader Board component
    Remove unused import

--- src/components/LeaderBoard.js
+++ src/components/LeaderBoard.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react'
+import React, { Fragment } from 'react'
 import { connect } from 'react-redux'
 import { Card, CardHeader, CardBody, CardImg, CardTitle, Row, Col, Badge } from 'reactstrap'
 
