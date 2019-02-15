74429cd0238924092b2998ded3c849a8391fd053
jsakowski <jsakowski@it.ucla.edu>
2019-02-15 12:02:55 -0800

    refactor: PollWrapper component
    Convert PollWrapper from class component to a functional component

--- src/components/PollWrapper.js
+++ src/components/PollWrapper.js
@@ -1,59 +1,45 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Card, CardHeader, CardBody, CardImg, Container, Row, Col, Button } from 'reactstrap';
 import { Link, withRouter } from 'react-router-dom'
 import PropTypes from 'prop-types'
 
+const PollWrapper = (props) => {
+  const { headerText, authorAvatar, isCancelButton } = props
+  const { returnTab } = props.location.state || { returnTab: undefined }
+  const returnState = returnTab !== undefined ? { returnTab: returnTab } : {}
 
-class PollWrapper extends Component {
-  static propTypes = {
-    headerText: PropTypes.string.isRequired,
-    authorAvatar: PropTypes.string.isRequired,
-    isCancelButton: PropTypes.bool,
-  }
+  return (
+    <Card>
+      <CardHeader>
+        {headerText}
+        {
+          isCancelButton &&
+          <Link to={{
+            pathname: '/',
+            state: returnState
+          }}><Button close /></Link>
+        }
+      </CardHeader>
+      <CardBody>
+        <Container>
+          <Row>
+            <Col xs={4} md={4}>
+              <CardImg className='img-avatar-medium' src={authorAvatar} />
+            </Col>
+            <Col xs={8}>
+              {props.children}
+            </Col>
+          </Row>
+        </Container>
+      </CardBody>
+    </Card>
+  )
+}
 
-  render () {
-    const { headerText, authorAvatar, isCancelButton} = this.props
-
-    let { returnTab } = this.props.location.state || { returnTab: undefined }
-
-    console.log('PollWrapper - render returnTab1', returnTab)
-
-    returnTab =
-      this.props === undefined || this.props.location === undefined || this.props.location.state === undefined
-        ? undefined
-        : this.props.location.state.returnTab
-
-    console.log('PollWrapper - render returnTab2', returnTab)
-
-    const returnState = returnTab !== undefined ? { returnTab: returnTab } : {}
-
-    return (
-      <Card>
-        <CardHeader>
-          {headerText}
-          {
-            isCancelButton &&
-            <Link to={{
-              pathname: '/',
-              state: returnState
-            }}><Button close /></Link>
-          }
-        </CardHeader>
-        <CardBody>
-          <Container>
-            <Row>
-              <Col xs={4} md={4}>
-                <CardImg className='img-avatar-medium' src={authorAvatar} />
-              </Col>
-              <Col xs={8}>
-                {this.props.children}
-              </Col>
-            </Row>
-          </Container>
-        </CardBody>
-      </Card>
-    )
-  }
+PollWrapper.propTypes = {
+  headerText: PropTypes.string.isRequired,
+  authorAvatar: PropTypes.string.isRequired,
+  isCancelButton: PropTypes.bool,
 }
 
 export default withRouter(PollWrapper)
