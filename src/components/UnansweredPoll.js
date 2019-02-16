7275d94b7ed7024468fa0f57d6fb6dbb02cdec64
jsakowski <jsakowski@it.ucla.edu>
2019-02-16 01:30:47 -0800

    feat: change radio button on vote a poll form

--- src/components/UnansweredPoll.js
+++ src/components/UnansweredPoll.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import { CardTitle, CardBody, Button, Form, FormGroup, Label, Input } from 'reactstrap'
+import { CardTitle, CardBody, Button, Form, FormGroup, Label, CustomInput } from 'reactstrap'
 
 class UnansweredPoll extends Component {
   static propTypes = {
@@ -37,22 +37,22 @@ class UnansweredPoll extends Component {
         <CardTitle tag='h1'>Would you rather...</CardTitle>
         <Form id='poll' onSubmit={this.handleSubmit}>
           <FormGroup key='optionOne' check>
-            <Input
+            <CustomInput
               type='radio'
               name='answerGroup'
               id='optionOne'
               onChange={this.handleChange}
+              label={poll.optionOne.text}
               required />
-            <Label check>{poll.optionOne.text}</Label>
           </FormGroup>
           <FormGroup key='optionTwo' className='mt-2' check>
-            <Input
+            <CustomInput
               type='radio'
               name='answerGroup'
               id='optionTwo'
               onChange={this.handleChange}
+              label={poll.optionTwo.text}
               required />
-            <Label check>{poll.optionTwo.text}</Label>
           </FormGroup>
           <Button className='mt-4 btn-block' color='primary' type='submit'>Submit</Button>
         </Form>
